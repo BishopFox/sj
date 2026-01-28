d9fac2e9fefa984076d4f810a6e5d8e12b3b81c5
Tony West <125916820+twest-bf@users.noreply.github.com>
2026-01-28 14:30:49 -0500

    Updated version string

--- cmd/root.go
+++ cmd/root.go
@@ -49,7 +49,7 @@ $ sj convert -u https://petstore.swagger.io/v2/swagger.json -o openapi.json`,
 			log.Error("Command not specified. See the --help flag for usage.")
 		}
 	},
-	Version: "2.1.0",
+	Version: "2.2.0",
 }
 
 func Execute() {
